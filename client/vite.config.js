e8dd8b100364af5e38970461ae694aaf7ebe41e4
user <user@local>
2026-02-05 20:07:37 +0100

    fix docker-compose.yml

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5174,
-    host: true
-  }
-})
+    watch: {
+      usePolling: true,
+    },
+  },
+});
