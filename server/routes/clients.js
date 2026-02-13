f6bc2d81747afa133af33a9263d8b0aca468684b
user <user@local>
2026-02-13 20:04:29 +0100

    add adminMiddelware

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -1,27 +1,28 @@
 import express from "express";
 import { getDB } from "../db.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { adminMiddleware } from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, async (req, res) => {
-    try {
-        if (req.user.role !== "admin") {
-            return res.status(403).json({ message: "Forbidden" });
+router.get(
+    "/",
+    authMiddleware,
+    adminMiddleware,
+    async (req, res) => {
+        try {
+            const db = getDB();
+
+            const [rows] = await db.query(
+                "SELECT id, name, email, role, created_at FROM users"
+            );
+
+            res.json(rows);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: "Server error" });
         }
-
-        const db = getDB();
-
-        const [rows] = await db.query(
-            "SELECT id, name, email, role, created_at FROM users"
-        );
-
-        res.json(rows);
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server error" });
     }
-});
+);
 
 export default router;
